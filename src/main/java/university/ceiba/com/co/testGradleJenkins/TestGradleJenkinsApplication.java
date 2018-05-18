589871547a1d3be15ca1c34bcefaaa398f7a9fca
Guillermo Díaz Villegas - Ceiba Software <guillermo.diaz@MacBook-Pro-de-Soporte.local>
2018-05-18 14:32:23 -0500

    first commit

---
+++ src/main/java/university/ceiba/com/co/testGradleJenkins/TestGradleJenkinsApplication.java
@@ -0,0 +1,12 @@
+package university.ceiba.com.co.testGradleJenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGradleJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestGradleJenkinsApplication.class, args);
+	}
+}
